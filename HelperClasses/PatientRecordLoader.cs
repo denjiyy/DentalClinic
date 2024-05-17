764a530e2ca6e27ae81cb8da33c518a683a77250
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-17 21:25:17 +0300

    Add files via upload

---
+++ HelperClasses/PatientRecordLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using DentalClinicManagement.Classes;
+
+namespace DentalClinicManagement.LessImportantClasses
+{
+    public class PatientRecordLoader
+    {
+        private readonly string? _filePath;
+
+        public PatientRecordLoader(string? filePath)
+        {
+            _filePath = filePath;
+        }
+        public void Save(List<Patient> patientRecords)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Patient>));
+            using FileStream? stream = _filePath != null ? new FileStream(_filePath, FileMode.Create) : null;
+            if (stream != null)
+            {
+                serializer.Serialize(stream, patientRecords);
+                stream.Close();
+            }
+        }
+
+        public List<Patient> Load()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Patient>));
+            using FileStream? stream = _filePath != null ? new FileStream(_filePath, FileMode.Open) : null;
+            if (stream == null)
+            {
+                return new List<Patient>();
+            }
+            List<Patient>? records = (List<Patient>?)serializer.Deserialize(stream);
+            return records ?? new List<Patient>();
+        }
+    }
+}
