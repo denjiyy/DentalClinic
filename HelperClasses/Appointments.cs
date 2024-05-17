46d4a8e3a1dd6ddf9ad75e9fb5eaef7af98e8004
Denis <125385668+denjiyy@users.noreply.github.com>
2024-05-17 21:24:14 +0300

    Add files via upload

---
+++ HelperClasses/Appointments.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinicManagement.LessImportantClasses
+{
+    internal class Appointments
+    {
+        public void AddPatient(string query)
+        {
+            Connection connection = new Connection();
+            SqlConnection conn = connection.GetConnection();
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            conn.Open();
+            command.CommandText = query;
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void DeletePatient(string query)
+        {
+            Connection connection = new Connection();
+            SqlConnection conn = connection.GetConnection();
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            conn.Open();
+            command.CommandText = query;
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void UpdatePatient(string query)
+        {
+            Connection connection = new Connection();
+            SqlConnection conn = connection.GetConnection();
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            conn.Open();
+            command.CommandText = query;
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public DataSet ShowPatient(string query)
+        {
+            Connection connection = new Connection();
+            SqlConnection conn = connection.GetConnection();
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = query;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            return dataSet;
+        }
+    }
+}
